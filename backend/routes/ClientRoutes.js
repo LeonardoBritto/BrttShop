a883d80b5d78a2e1cda43566f1982905999d9e21
Leonardo Brito <leonardobrito992@gmail.com>
2024-08-31 12:03:46 -0300

    Ajustando helpers(Adjusting helpers).

--- backend/routes/ClientRoutes.js
+++ backend/routes/ClientRoutes.js
@@ -7,7 +7,7 @@ const verifyToken = require('../helpers/verify-token')
 router.post('/create',          ClientController.create)
 router.patch('/update/:id',     verifyToken,    ClientController.update)
 router.delete('/delete/:id',    verifyToken, ClientController.delete)
-router.post('/login',           verifyToken, ClientController.login)
+router.post('/login',           ClientController.login)
 router.get('/:id',              verifyToken, ClientController.getById)
 router.get('/',                 verifyToken, ClientController.getAll)
 
